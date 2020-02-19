ef37cb116b82b6c957cf5119f605f08db29e7181
Donat Pllana <donat.pllana@outlook.com>
2020-02-19 02:52:33 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist={eminem:"goat"};
+function updatePlaylist(playlist, artistName, songTitle)
+{
+  return playlist[artistName]="songTitle";
+}
+function removeFromPlaylist(playlist, artistName)
+{
+  return delete playlist.eminem;
+}
